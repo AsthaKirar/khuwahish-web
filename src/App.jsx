26118ac7bf5fc507b103890033c5fae4d927d133
AsthaKirar <asthap602@gmail.com>
2025-01-20 23:17:55 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <div className="">
+     
+      <Banner />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="relative min-h-screen w-full bg-[url('src/assets/assai.png')]" >
+      <div className="w-full h-[90vh]">
+        <h1  style={{ fontFamily: "Carpethen" }} className="text-[40vh] text-white ml-[30%] mt-0">Aasai
+        </h1>
+        <p className="text-2xl text-white ml-[15%]">Recrafting traditions: Experience heritage jewelry reinterpreted with exquisite artistry <br /> and design prowess, and be your royal self as you adorn these regal pieces.
+        </p>
+      </div>
+      <h1 style={{ fontFamily: "Carpethen" }} className="text-8xl  ml-[30%] text-white">
+        The Maanga Mala
+      </h1>
+
+      <div className="w-full h-[90vh] relative pt-10 flex justify-center gap-[10%]">
+        <div className="w-[40%] ml-[5%] rounded-2xl">
+          <img className="rounded-3xl object-cover h-full w-full shadow-2xl" src="src/assets/image.png" alt="image" />
+        </div>
+        <div className="w-[45%]">
+          <p className="pt-[30%] p-10 text-white">
+            The Maanga Malai is an important wedding gold necklace for <br />
+            women in the South. Our designs are a combination of precious <br />
+            gemstones and diamonds in a contemporary take. It is sometimes <br />
+            referred to as the temple jewellery of the South.
+          </p>
+          <div className="ml-10 w-[50%] h-[10%] hover:bg-transparent  bg-white hover:text-white rounded-full   relative flex gap-8 items-center  border-2 border-solid border-white">
+<div className="w-[7vh] h-[7vh] hover:bg-white bg-[#5D3A1C] rounded-[50%] relative ml-6 radius-[50%]">
+<i className="ri-arrow-right-line text-2xl hover:text-yellow-300 text-white absolute left-2 top-1 "></i>
+
+</div>
+            <h1 className="text-xl ">View Collection</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full h-screen ">
+      <h1 style={{ fontFamily: "Carpethen" }} className="text-8xl ml-[30%] text-white mt-[5%]">
+        The Kaasu Malai
+      </h1>
+      <div className="w-full h-[90vh] relative pt-10 flex justify-center gap-[10%]">
+        <div className="w-[45%] ml-[5%]">
+          <p className="pt-[30%] p-10 text-white text-20px">
+          The Kaasu Malai is a significant piece of the wedding trousseau of a South Indian bride and is manufactured by stringing identical coins extremely closely together. The etching on each coin could represent a variety of things, like the era it was made, Goddess Lakshmi's auspiciousness, or the beauty of flora and animals.
+
+          </p>
+          <div className="ml-10 w-[50%] h-[10%] hover:bg-transparent  bg-white hover:text-white rounded-full   relative flex gap-8 items-center  border-2 border-solid border-white">
+<div className="w-[7vh] h-[7vh] hover:bg-white bg-[#5D3A1C] rounded-[50%] relative ml-6 radius-[50%]">
+<i className="ri-arrow-right-line text-2xl hover:text-yellow-300 text-white absolute left-2 top-1 "></i>
+
+</div>
+
+            <h1 className="text-xl">View Collection</h1>
+          </div>
+        </div>
+        <div className="w-[45%] mr-[5%] rounded-2xl">
+          <img className="rounded-3xl object-cover h-full w-full" src="src/assets/img-2.jpg" alt="image" />
+        </div>
+
+      
+      </div>
+
+
+
+
+      </div>
+      <div className="w-full h-screen">
+      <h1 style={{ fontFamily: "Carpethen" }} className="text-8xl ml-[30%] text-white mt-[5%]">
+      Temple Jewellery
+
+      </h1>
+
+      <div className="w-full h-[90vh] relative pt-10 flex justify-center gap-[10%]">
+        <div className="w-[40%] ml-[5%] rounded-2xl">
+          <img className="rounded-3xl object-cover h-full w-full" src="src/assets/img-33.jpg" alt="image" />
+        </div>
+        <div className="w-[50%]">
+          <p className="pt-[25%] p-10 text-white text-25px">
+          Temple Jewellery is crafted using the age-old art of creating mythical creatures like fairies, gods, and goddesses out of gold sheets. It is made by skilled artisans and is embellished  with pearls and gemstones. This necklace is one of the very versatile Indian wedding jewellery forms, it can be layered with other necklaces, for a bridal look.
+
+          </p>
+          <div className="ml-10 w-[50%] h-[10%] hover:bg-transparent  bg-white hover:text-white rounded-full   relative flex gap-8 items-center  border-2 border-solid border-white">
+<div className="w-[7vh] h-[7vh] hover:bg-white bg-[#5D3A1C] rounded-[50%] relative ml-6 radius-[50%]">
+<i className="ri-arrow-right-line text-2xl hover:text-yellow-300 text-white absolute left-2 top-1 "></i>
+
+</div>
+
+            <h1 className="text-xl">View Collection</h1>
+          </div>
+        </div>
+      </div>
+
+      </div>
+      <div className="w-full min-h-screen">
+      <h1 style={{ fontFamily: "Carpethen" }} className="text-8xl ml-[40%] text-white mt-[5%]">
+        Jhumka
+      </h1>
+      <div className="w-full h-[90vh] relative pt-10 flex justify-center gap-[10%]">
+        <div className="w-[45%] ml-[5%]">
+          <p className="pt-[30%] p-10 text-white text-20px">
+          Jhumka earrings a timeless jewellery ornament, which is always in fashion. They are well known jewellery ornament from royal emperor era till now. Contemporary or Traditionally styled, it is India’s favourite earring style be it South or North India.
+
+          </p>
+          <div className="ml-10 w-[50%] h-[10%] hover:bg-transparent  bg-white hover:text-white rounded-full   relative flex gap-8 items-center  border-2 border-solid border-white">
+<div className="w-[7vh] h-[7vh] hover:bg-white bg-[#5D3A1C] rounded-[50%] relative ml-6 radius-[50%]">
+<i className="ri-arrow-right-line text-2xl hover:text-yellow-300 text-white absolute left-2 top-1 "></i>
+
+</div>
+
+     
+            <h1 className="text-xl">View Collection</h1>
+          </div>
+        </div>
+        <div className="w-[45%] mr-[5%] rounded-2xl">
+          <img className="rounded-3xl object-cover h-full w-full" src="src/assets/img-4.jpg" alt="image" />
+        </div>
+
+      
+      </div>
+
+
+<h2  className="text-4xl ml-[30%] p-[5%] text-white">__Our Ad Campaign__
+</h2>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
